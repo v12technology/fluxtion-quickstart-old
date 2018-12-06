759ee9eeeccd6691386ef2a0f19c91c01cfdd6db
greg higgins <greg.higgins@v12technology.com>
2018-12-06 23:50:02 +0000

    added more to README. Updated pom to using native java Fluxtion generator

--- src/main/java/com/fluxtion/sample/wordcount/generated/WcProcessor.java
+++ src/main/java/com/fluxtion/sample/wordcount/generated/WcProcessor.java
@@ -72,8 +72,8 @@ public class WcProcessor implements EventHandler, BatchHandler, Lifecycle {
         return;
     }
     //Default, no filter methods
-    result.onUnmatchedChar(typedEvent);
     result.onAnyChar(typedEvent);
+    result.onUnmatchedChar(typedEvent);
     //event stack unwind callbacks
     afterEvent();
   }
